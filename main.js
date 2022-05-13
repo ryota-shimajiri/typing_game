4fd739825a77f8a235c3ff3f2048b035e1392d9d
shimajiri <ryota04130413@gmail.com>
2022-05-13 15:53:51 +0900

    update

--- main.js
+++ main.js
@@ -1,7 +1,27 @@
-var questionList = ["apple", "banana", "melon", "orange"];
-var randomNum = Math.floor(Math.random() * questionList.length);
-document.addEventListener("keydown", function (e) {
-    var keys = e.key;
-    document.getElementById("test").innerHTML = keys;
+var questionList = ["apple", "banana", "melon", "orange", "grape", "coconut", "strawberry"], startBtn = document.getElementById("start");
+var randomNum = Math.floor(Math.random() * questionList.length), textLength = questionList[randomNum].length, currentPosition = 0, startFlg = false;
+startBtn.addEventListener("click", start);
+function start() {
+    init();
+    startFlg = true;
     document.getElementById("questionText").innerHTML = questionList[randomNum];
+}
+document.addEventListener("keydown", function (e) {
+    if (startFlg === false) {
+        return;
+    }
+    var key = e.key, questionText = questionList[randomNum];
+    if (key === questionText.charAt(currentPosition)) {
+        currentPosition++;
+        document.getElementById("questionText").innerHTML = questionText.substring(currentPosition, textLength);
+        if (currentPosition === textLength) {
+            init();
+            document.getElementById("questionText").innerHTML = questionList[randomNum];
+        }
+    }
 });
+function init() {
+    randomNum = Math.floor(Math.random() * questionList.length),
+        textLength = questionList[randomNum].length;
+    currentPosition = 0;
+}
--- main.ts
+++ main.ts
@@ -1,8 +1,41 @@
-const questionList = ["apple", "banana", "melon", "orange"];
-const randomNum = Math.floor(Math.random() * questionList.length);
+const questionList = ["apple", "banana", "melon", "orange", "grape", "coconut", "strawberry"], 
+      startBtn = document.getElementById("start");
+let randomNum = Math.floor(Math.random() * questionList.length),
+    textLength:number = questionList[randomNum].length,
+    currentPosition:number = 0,
+    startFlg:boolean = false;
+
+startBtn.addEventListener("click", start);
+
+function start() {
+    init();
+    startFlg = true;
+    document.getElementById("questionText").innerHTML = questionList[randomNum];
+}
 
 document.addEventListener("keydown", (e) => {
-    const keys = e.key;
-    document.getElementById("test").innerHTML = keys;
-    document.getElementById("questionText").innerHTML = questionList[randomNum];
+
+    if (startFlg === false) {
+        return;
+    }
+    
+    const key = e.key,
+          questionText = questionList[randomNum];
+
+    if(key === questionText.charAt(currentPosition)) {
+        currentPosition++;
+        document.getElementById("questionText").innerHTML = questionText.substring(currentPosition, textLength);
+
+        if (currentPosition === textLength) {
+
+            init();
+            document.getElementById("questionText").innerHTML = questionList[randomNum];
+        }
+    }
 });
+
+function init() {
+    randomNum = Math.floor(Math.random() * questionList.length),
+    textLength = questionList[randomNum].length;
+    currentPosition = 0;
+}
