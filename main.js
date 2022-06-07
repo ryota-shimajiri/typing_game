cfdd04241db9b7ce560fca87395bd4a160c1e9b5
shimajiri <ryota04130413@gmail.com>
2022-06-07 17:50:39 +0900

    update

--- main.js
+++ main.js
@@ -4,24 +4,29 @@ startBtn.addEventListener("click", start);
 function start() {
     init();
     startFlg = true;
-    document.getElementById("questionText").innerHTML = questionList[randomNum];
+    document.getElementById("typingText").innerHTML = questionList[randomNum];
 }
 document.addEventListener("keydown", function (e) {
     if (startFlg === false) {
         return;
     }
-    var key = e.key, questionText = questionList[randomNum];
-    if (key === questionText.charAt(currentPosition)) {
-        currentPosition++;
-        document.getElementById("questionText").innerHTML = questionText.substring(currentPosition, textLength);
-        if (currentPosition === textLength) {
-            init();
-            document.getElementById("questionText").innerHTML = questionList[randomNum];
-        }
-    }
+    typing(e);
 });
 function init() {
     randomNum = Math.floor(Math.random() * questionList.length),
         textLength = questionList[randomNum].length;
     currentPosition = 0;
+}
+function typing(e) {
+    var key = e.key, typingText = questionList[randomNum];
+    if (key === typingText.charAt(currentPosition)) {
+        currentPosition++;
+        // 先頭の文字を切り取る
+        document.getElementById("typingText").innerHTML = typingText.substring(currentPosition, textLength);
+        if (currentPosition === textLength) {
+            // 最後の文字を打ち終えたら
+            init();
+            document.getElementById("typingText").innerHTML = questionList[randomNum];
+        }
+    }
 }
--- main.ts
+++ main.ts
@@ -1,16 +1,16 @@
-const questionList = ["apple", "banana", "melon", "orange", "grape", "coconut", "strawberry"], 
-      startBtn = document.getElementById("start");
-let randomNum = Math.floor(Math.random() * questionList.length),
-    textLength:number = questionList[randomNum].length,
-    currentPosition:number = 0,
-    startFlg:boolean = false;
+const questionList : string[] = ["apple", "banana", "melon", "orange", "grape", "coconut", "strawberry"],
+    startBtn = document.getElementById("start") as HTMLElement;
+let randomNum: number = Math.floor(Math.random() * questionList.length),
+    textLength: number = questionList[randomNum].length,
+    currentPosition: number = 0,
+    startFlg: boolean = false;
 
 startBtn.addEventListener("click", start);
 
 function start() {
     init();
     startFlg = true;
-    document.getElementById("questionText").innerHTML = questionList[randomNum];
+    document.getElementById("typingText").innerHTML = questionList[randomNum];
 }
 
 document.addEventListener("keydown", (e) => {
@@ -18,24 +18,28 @@ document.addEventListener("keydown", (e) => {
     if (startFlg === false) {
         return;
     }
-    
-    const key = e.key,
-          questionText = questionList[randomNum];
-
-    if(key === questionText.charAt(currentPosition)) {
-        currentPosition++;
-        document.getElementById("questionText").innerHTML = questionText.substring(currentPosition, textLength);
-
-        if (currentPosition === textLength) {
-
-            init();
-            document.getElementById("questionText").innerHTML = questionList[randomNum];
-        }
-    }
+    typing(e);
 });
 
 function init() {
     randomNum = Math.floor(Math.random() * questionList.length),
-    textLength = questionList[randomNum].length;
+        textLength = questionList[randomNum].length;
     currentPosition = 0;
+}
+
+function typing(e: KeyboardEvent) {
+    const key: string = e.key,
+    typingText = questionList[randomNum];
+
+    if (key === typingText.charAt(currentPosition)) {
+        currentPosition++;
+        // 先頭の文字を切り取る
+        document.getElementById("typingText").innerHTML = typingText.substring(currentPosition, textLength);
+
+        if (currentPosition === textLength) {
+            // 最後の文字を打ち終えたら
+            init();
+            document.getElementById("typingText").innerHTML = questionList[randomNum];
+        }
+    }
 }
